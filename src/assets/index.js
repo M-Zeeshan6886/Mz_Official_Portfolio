8913a9c978743318c98b43ab4917d8edb5d0a736
M-Zeeshan6886 <mzxeeshuu@gmail.com>
2025-09-04 12:46:24 -0700

    update

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,8 @@
 import Logo from "./Logo2.png";
 import heroBanner from "./heroBanner.png";
+import SIcon1 from "./serviceIcon1.svg";
+import SIcon2 from "./serviceIcon2.svg";
+import SIcon3 from "./serviceIcon3.svg";
+import SIcon4 from "./serviceIcon4.svg";
 
-export { Logo, heroBanner };
+export { Logo, heroBanner, SIcon1, SIcon2, SIcon3, SIcon4 };
--- src/modules/home/Home.jsx
+++ src/modules/home/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Navbar } from "../../common";
 import styles from "./Home.module.scss";
 import { MdOutlineFileDownload } from "react-icons/md";
-import { heroBanner } from "../../assets";
+import { heroBanner, SIcon1, SIcon2, SIcon3, SIcon4 } from "../../assets";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 
@@ -59,8 +59,60 @@ const Home = () => {
           </div>
         </div>
       </div>
+      <div className={styles.service_container}>
+        <div className={styles.service_container_content}>
+          <div className={styles.service_container_content_top}>
+            <h2>Expertise In</h2>
+            <h1>
+              These <span className={styles.primaryOutline}>Technologies </span>
+            </h1>
+            <p>
+              I'm not just about developing software. I generate breakthrough
+              solutions that rewrite the digital rules by igniting creative
+              encounters. Let's collaborate to create innovative experiences
+              that surpass expectations.
+            </p>
+          </div>
+          <div className={styles.service_container_content_bottom}>
+            <ServiceBox
+              ImgIcon={SIcon1}
+              title="React JS"
+              para="Build scalable, responsive web apps with reusable components and smooth user experiences."
+            />
+            <ServiceBox
+              ImgIcon={SIcon2}
+              title="JavaScript"
+              para="Write clean, modern JavaScript to power fast, interactive, and reliable applications. "
+            />
+            <ServiceBox
+              ImgIcon={SIcon3}
+              title="Bootstrap"
+              para="Create responsive, consistent layouts that look great across all devices."
+            />
+            <ServiceBox
+              ImgIcon={SIcon4}
+              title="HTML||CSS||SCSS"
+              para="Craft clean, accessible designs with structured code and modern styling."
+            />
+          </div>
+        </div>
+      </div>
     </>
   );
 };
 
 export default Home;
+
+const ServiceBox = (props) => {
+  return (
+    <>
+      <div className={styles.sBox}>
+        <div className={styles.sBox_content}>
+          <img src={props.ImgIcon} alt="Icons" />
+          <h1>{props.title}</h1>
+          <p>{props.para}</p>
+        </div>
+      </div>
+    </>
+  );
+};
