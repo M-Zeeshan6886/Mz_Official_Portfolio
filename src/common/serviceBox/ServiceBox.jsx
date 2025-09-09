8105f1fb775136ab6b352a2ce148f87ec719700c
M-Zeeshan6886 <mzxeeshuu@gmail.com>
2025-09-09 12:00:32 -0700

    'update'

--- src/common/serviceBox/ServiceBox.jsx
+++ src/common/serviceBox/ServiceBox.jsx
@@ -1,34 +1,26 @@
-import React, { useState } from "react";
-import ReactCardFlip from "react-card-flip";
+import React from "react";
 import styles from "./ServiceBox.module.scss";
 
 const ServiceBox = ({ ImgIcon, title, para, backContent }) => {
-  const [isFlipped, setIsFlipped] = useState(false);
-
   return (
-    <div
-      className={styles.flipCard}
-      onMouseEnter={() => setIsFlipped(true)}
-      onMouseLeave={() => setIsFlipped(false)}
-    >
-      <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
-        {/* Front Side */}
-        <div className={styles.sBox}>
+    <div className={styles.flipCard}>
+      <div className={styles.flipCardInner}>
+        {/* Front */}
+        <div className={`${styles.sBox} ${styles.front}`}>
           <div className={styles.sBox_content}>
             <img src={ImgIcon} alt="Icons" className={styles.serviceIcons} />
             <h1>{title}</h1>
           </div>
         </div>
 
-        {/* Back Side */}
+        {/* Back */}
         <div className={`${styles.sBox} ${styles.back}`}>
           <div className={styles.sBox_content}>
             <h1>{title}</h1>
-            <p></p>
-            <p>{backContent || `${para}`}</p>
+            <p>{backContent || para}</p>
           </div>
         </div>
-      </ReactCardFlip>
+      </div>
     </div>
   );
 };
