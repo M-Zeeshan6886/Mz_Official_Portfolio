365e320a2ae82160cd273fbb66b0747db58484d9
M-Zeeshan6886 <mzxeeshuu@gmail.com>
2025-09-10 13:52:31 -0700

    update

--- src/common/exploreWork/Explore.jsx
+++ src/common/exploreWork/Explore.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import styles from "./Explore.module.scss";
 
 const Explore = () => {
   return (
-    <div>Explore</div>
-  )
-}
+    <>
+      <div className={styles.exploreWork_container}>
+        <div className={styles.exploreWork_container_content}>
+          <span>OUR PORTFOLIO</span>
+          <h1>
+            <span>EXPLORE</span> <h2>OUR</h2> WORK
+          </h1>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Explore
+export default Explore;
