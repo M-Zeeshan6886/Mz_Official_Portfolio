3b6e497d756aac6d9ae0e3f8fd5358ba84e0cf52
M-Zeeshan6886 <mzxeeshuu@gmail.com>
2025-09-10 14:15:00 -0700

    update

--- src/assets/index.js
+++ src/assets/index.js
@@ -7,6 +7,7 @@ import SIcon4 from "./serviceIcon4.svg";
 import SIcon5 from "./serviceIcon5.svg";
 import SIcon6 from "./serviceIcon6.svg";
 import SIcon7 from "./serviceIcon7.svg";
+import EWF1 from "./ewframe1.png";
 
 export {
   Logo,
@@ -18,4 +19,5 @@ export {
   SIcon5,
   SIcon6,
   SIcon7,
+  EWF1,
 };
--- src/common/exploreWork/Explore.jsx
+++ src/common/exploreWork/Explore.jsx
@@ -1,15 +1,30 @@
 import React from "react";
 import styles from "./Explore.module.scss";
+import { EWF1 } from "../../assets";
 
 const Explore = () => {
   return (
     <>
       <div className={styles.exploreWork_container}>
         <div className={styles.exploreWork_container_content}>
-          <span>OUR PORTFOLIO</span>
+          <span>MY PORTFOLIO</span>
           <h1>
-            <span>EXPLORE</span> <h2>OUR</h2> WORK
+            <span>EXPLORE</span> <h2>MY</h2> WORK
           </h1>
+          <div className={styles.exploreWork_container_content_scroller}>
+            <img
+              src={EWF1}
+              alt="Frame"
+            //   onClick={() => navigate("/")}
+            //   className={currentPath === "/" ? styles.active : ""}
+            />
+            <img
+              src={EWF1}
+              alt="Frame"
+            //   onClick={() => navigate("/")}
+            //   className={currentPath === "/" ? styles.active : ""}
+            />
+          </div>
         </div>
       </div>
     </>
