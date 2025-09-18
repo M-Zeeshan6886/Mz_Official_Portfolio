16fbbf2ba0ca02fe97b2cb7265b09ae850995b80
M-Zeeshan6886 <mzxeeshuu@gmail.com>
2025-09-18 14:17:53 -0700

    updates

--- src/modules/about/About.jsx
+++ src/modules/about/About.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { Navbar } from "../../common";
 import styles from "./About.module.scss";
 import { MdOutlineFileDownload } from "react-icons/md";
+import { EWF5, EWF7, EWF8 } from "../../assets";
 
 const About = () => {
   useEffect(() => {
@@ -45,8 +46,51 @@ const About = () => {
           <div className={styles.about_hero_content_right}></div>
         </div>
       </div>
+      <div className={styles.experience_container}>
+        <div className={styles.experience_container_content}>
+          <div className={styles.experience_container_content_top}>
+            <h2>Relavent</h2>
+            <h1>
+              <span className={styles.primaryOutline}>Experience </span>
+            </h1>
+          </div>
+          <div className={styles.experience_container_content_bottom}>
+            <ExCard
+              title="Full Stack Developer"
+              comName="Peri Studios -- Full-time"
+              dates="DEC 2023 - Prensent 1 yr 6 mons"
+              P1="E-Learning Management System | SmartaClass"
+              P2="Smart Building Management System | Suren"
+              P3="Rental Management System | Sand Caddie"
+            />
+          </div>
+        </div>
+      </div>
     </>
   );
 };
 
 export default About;
+
+const ExCard = (props) => {
+  return (
+    <>
+      <div className={styles.excard_container}>
+        <div className={styles.excard_container_content}>
+          <img src={EWF5} alt="Logo" />
+          <div className={styles.excard_container_content_right}>
+            <h1>{props.title}</h1>
+            <h3>{props.comName}</h3>
+            <span>{props.dates}</span>
+            <br />
+            <br />
+            <p>Project : {props.P1}</p>
+            <p>Project : {props.P2}</p>
+            <p>Project : {props.P3}</p>
+            <p>Project : E-Commerce Management System | Rug Bazaar</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
