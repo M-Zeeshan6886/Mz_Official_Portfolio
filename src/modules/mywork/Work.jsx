1f642caf982aa8280b2ba25bcf5869fd87d04324
M-Zeeshan6886 <mzxeeshuu@gmail.com>
2025-09-20 13:44:19 -0700

    updates

--- src/modules/mywork/Work.jsx
+++ src/modules/mywork/Work.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Navbar } from '../../common';
+import styles from "./Work.module.scss";
+
 
 const Work = () => {
   return (
-    <div>Work</div>
+    <>
+    <Navbar />
+    <div className={styles.work_hero}>
+        <div className={styles.work_hero_content}>
+            
+        </div>
+    </div>
+    </>
   )
 }
 
