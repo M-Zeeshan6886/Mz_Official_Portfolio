09fd76a0fde0d7a21288705d0b42a438828847f5
M-Zeeshan6886 <mzxeeshuu@gmail.com>
2025-09-21 10:24:06 -0700

    updates

--- src/modules/mywork/Work.jsx
+++ src/modules/mywork/Work.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
-import { Navbar } from '../../common';
+import React from "react";
+import { Navbar } from "../../common";
 import styles from "./Work.module.scss";
-
 
 const Work = () => {
   return (
     <>
-    <Navbar />
-    <div className={styles.work_hero}>
+      <Navbar />
+      <div className={styles.work_hero}>
         <div className={styles.work_hero_content}>
-            
+          <h1>
+            My <span className={styles.primaryOutline}>Work </span>
+          </h1>
         </div>
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Work
+export default Work;
