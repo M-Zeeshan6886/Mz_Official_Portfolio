0085912487d2820951181f3c63ce17d21cb5caa3
M-Zeeshan6886 <mzxeeshuu@gmail.com>
2025-09-23 09:34:29 -0700

    updates

--- src/modules/mywork/Work.jsx
+++ src/modules/mywork/Work.jsx
@@ -11,7 +11,22 @@ const Work = () => {
           <h1>
             My <span>Work </span>
           </h1>
-          
+          <p>
+            🚀 Showcasing my web development projects built with modern
+            technologies. 🌐 From responsive UI/UX to dynamic applications, each
+            project highlights my skills. 💡 Focused on creating clean,
+            user-friendly, and impactful digital solutions.
+          </p>
+          <div className={styles.work_hero_content_top}>
+            <button>Rug Bazaar</button>
+            <button>Sand Caddie</button>
+            <button>Suren</button>
+            <button>SmartaClass</button>
+            <button>AI ChatBot</button>
+            <button>Punt App</button>
+            <button>Signature</button>
+            <button>Peri Studios</button>
+          </div>
         </div>
       </div>
     </>
