c079ec1f62a1392b56969a184f7980f1db5092d0
M-Zeeshan6886 <mzxeeshuu@gmail.com>
2025-10-09 13:02:49 -0700

    updates

--- src/modules/mywork/workDec/SurenDec.jsx
+++ src/modules/mywork/workDec/SurenDec.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import styles from "../Work.module.scss";
+import { EWB3 } from "../../../assets";
 
 const SurenDec = () => {
   return (
-    <div>SurenDec</div>
-  )
-}
+    <>
+      <div className={styles.dec_container}>
+        <h1>Smart Building Management System</h1>
+        <div className={styles.dec_container_content}>
+          <div className={styles.dec_container_content_left}>
+            <img src={EWB3} alt="" />
+          </div>
+          <div className={styles.dec_container_content_right}></div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default SurenDec
+export default SurenDec;
